b5fa1381add5d8045c3fb641c11cd73327d8bf65
= <=>
2023-11-13 18:54:00 +0530

    passed LocationOfExpenditure as a prob with other

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import ExpenseItem from './component/ExpenseItem/ExpenseItem';
 
 function App() {
  let data = [
-    {itemName:"Item1", itemDisc:"This is discri", itemDate:"10-12-2022", itemRate:1200},
-    {itemName:"Item2", itemDisc:"This is discri", itemDate:"20-09-2023", itemRate:1000},
-    {itemName:"Item3", itemDisc:"This is discri", itemDate:"20-05-2021", itemRate:1500}
+    {itemName:"Laptop", LocationOfExpenditure:"Delhi", itemDate:"10-12-2022", itemRate:120000},
+    {itemName:"Mobile", LocationOfExpenditure:"Jaipur", itemDate:"20-09-2023", itemRate:10000},
+    {itemName:"Screen", LocationOfExpenditure:"Raipur", itemDate:"20-05-2021", itemRate:1500}
   ]
- 
+
   return (
     data.map( (data ) => {
-      return (<ExpenseItem itemName={data.itemName} itemDesc ={data.itemDisc} itemDate = {data.itemDate} itemRate = {data.itemRate} />)
+      return (<ExpenseItem itemName={data.itemName} itemDesc ={data.LocationOfExpenditure} itemDate = {data.itemDate} itemRate = {data.itemRate} />)
     })
      
   );
--- src/component/ExpenseItem/ExpenseItem.js
+++ src/component/ExpenseItem/ExpenseItem.js
@@ -2,18 +2,14 @@ import "./ExpenseItem.css"
 
 function ExpenseItem(props){
     return( 
-
        <div className="expenseContainer">
-        <div className="expenseItem expense">
-        <div>{props.itemName}</div>
-        <div className="descri">{props.itemDesc}</div>
-        </div>
+        <div className="expenseName expense">{props.itemName}</div>
+        <div className="descri expense">{props.itemDesc}</div>
         <div className="expenseDate expense">{props.itemDate}</div>
         <div className="expenseRate expense">{props.itemRate}</div>
        </div>
        
     )
-
 }
 
 export default ExpenseItem;
