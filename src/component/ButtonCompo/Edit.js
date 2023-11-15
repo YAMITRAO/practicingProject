a6fe83d2cb5b73c185a078bea287a40413d6418a
= <=>
2023-11-15 09:37:22 +0530

    update button added with function of useState

--- src/component/ButtonCompo/Edit.js
+++ src/component/ButtonCompo/Edit.js
@@ -1,8 +1,13 @@
 import ButtonCard from "../UI/ButtonCard";
+import { useState } from "react";
 import "./Edit.css"
 
 const Edit = () => {
 
+    const [val, setVal] = useState("hello");
+    
+
+     
     const edited = () => {
         console.log("Edited")
     }
--- src/component/ExpenseItem/ExpenseDetails.js
+++ src/component/ExpenseItem/ExpenseDetails.js
@@ -1,22 +1,53 @@
 import "./ExpenseDetails.css";
+import ButtonCard from "../UI/ButtonCard"
+import { useState } from "react";
 
 
 const ExpenseDetails = (props) => {
+
+    const [name , setName] = useState(props.name);
+    const [location, setLocation] = useState(props.location);
+    const [rate, setRate] = useState(props.rate);
+  
+    
+    const nameUpdateFun = ( ) => {
+        let updatedValue = prompt("Updated name ");
+        if(updatedValue.length > 0){
+            setName(updatedValue);
+        }
+        
+    };
+    const locationUpdateFun = () => {
+        let updatedValue = prompt("Updated location");
+        if(updatedValue.length > 0){
+        setLocation(updatedValue);
+        }
+    };
+    const rateUpdateFun = () => {
+        let updatedValue = prompt("Updated rate value");
+        if(updatedValue.length > 0) {
+        setRate(updatedValue);
+        }
+    }
 
     return(
         <>
         <div>
             <div className="itemDes">Item</div>
-         <div className="expense">{props.name}</div>
+         <div className="expense">{name}</div>
+         <ButtonCard button={nameUpdateFun}className="updateButton">Update</ButtonCard>
         </div>
+
         <div>
         <div className="itemDes">Location</div>
-        <div className="expense">{props.location}</div>
+        <div className="expense">{location}</div>
+        <ButtonCard button={locationUpdateFun}className="updateButton">Update</ButtonCard>
         </div>
 
         <div>
         <div className="itemDes">price</div>
-        <div className="expense">{props.rate}</div>
+        <div className="expense">{rate}</div>
+        <ButtonCard button={rateUpdateFun}className="updateButton">Update</ButtonCard>
         </div>
         
         
